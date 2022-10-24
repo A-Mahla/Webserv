c727f27e618ebc47d1f5062a2fdda3fe19f5de0e
Maxence Eudier <eudier.maxence@gmail.com>
2022-10-24 13:01:52 +0200

    test mac version

--- headers/webserv.h
+++ headers/webserv.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   webserv.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amahla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: maxenceeudier <maxenceeudier@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 18:54:23 by amahla            #+#    #+#             */
-/*   Updated: 2022/10/24 01:01:25 by amahla           ###   ########.fr       */
+/*   Updated: 2022/10/24 11:36:40 by maxenceeudi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 # include "WebServException.hpp"
 # include "Server.hpp"
 # include "Client.hpp"
-# include <sys/epoll.h>
+//# include <sys/epoll.h>
 # include <vector>
 
 # define MAXFD 5000
 
-typedef struct s_epoll
+/*typedef struct s_epoll
 {
 	int					maxNbFd;
 	int					epollFd;
 	struct epoll_event	new_event;
 	struct epoll_event	events[MAXFD];
-}	t_epoll;
+}	t_epoll;*/
 
-typedef typename std::vector<Client>::iterator	itClient;
-typedef typename std::vector<Server>::iterator	itServer;
+typedef  std::vector<Client>::iterator	itClient;
+typedef  std::vector<Server>::iterator	itServer;
 
 			/*	socket_settings.cpp		*/
 void		nonBlockSock( int & servSock );
@@ -45,12 +45,12 @@ void		appServ( std::vector<Server> & servers );
 			/*	serv_process_epoll_utils.cpp	*/
 itClient	find( std::vector<Client> & clients, int clientFd );
 itServer	find( std::vector<Server> & servers, int serverFd );
-void		setEpollQueue( t_epoll & epollVar, std::vector<Server> & servers );
+//void		setEpollQueue( t_epoll & epollVar, std::vector<Server> & servers );
 void		signal_handler(int sig);
 int			isServer( std::vector<Server> & servers, int fd );
 
 			/*	serv_process_epoll_loop	*/
-void		servProcess( std::vector<Server> & servers,
-				std::vector<Client> & clients, t_epoll & epollVar );
+//void		servProcess( std::vector<Server> & servers,
+//				std::vector<Client> & clients, t_epoll & epollVar );
 
 #endif
